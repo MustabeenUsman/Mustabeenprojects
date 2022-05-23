a41ecea43f51dddf00c3da2c9a5135a5d1a37f36
Mustabeen Usman <mustabeenusman12@gmail.com>
2022-05-23 15:45:57 +0500

    second comit

--- fyp/fyp/settings.py
+++ fyp/fyp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m)$yy7$zur-exr!+e^muh+6a2j=m%fb&d_v$5@hpd8v7+b35^r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
